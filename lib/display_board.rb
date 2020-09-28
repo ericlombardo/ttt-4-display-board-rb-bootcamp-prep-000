0ab70e5ad96a7db1d0acf9fc6edf48741223da85
Eric Lombardo <eric.m.lombardo@gmail.com>
2020-09-28 18:39:28 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,7 +5,7 @@ method display_board(board)
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  puts " #{board[a]} | #{board[7]} | #{board[8]} "
 end
 
 display_board(board)
